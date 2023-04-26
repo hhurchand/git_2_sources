7a34156c4f9e2e5c1400c39deab9ac3c171c4d09
hhurchan <h.hurchand@gmail.com>
2023-04-26 19:20:40 -0400

    Add grocery items

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("Hello")
+def grocery_list():
+    grocery = ["Milk",
+               "Bread",
+               "Eggs"]
+    return grocery
+
+
+if __name__ == "__main__":
+    print("Display grocery list")
+    print(grocery_list())
