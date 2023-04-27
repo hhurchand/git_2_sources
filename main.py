ed4f9f9bc1bdcceb24df31fbf47b200306d24a33
hhurchan <h.hurchand@gmail.com>
2023-04-27 19:23:03 -0400

    resolved conflict

--- main.py
+++ main.py
@@ -7,25 +7,7 @@ def grocery_list():
                "jam"]
     return grocery
 
-def clothing():
-    clothing_list = ["shirt",
-    "dress",
-    "socks",
-    "tie"
-    ]
-    return clothing_list
-
-def bills():
-    bills_items = ["electricity",
-    "laundry",
-    "travel",
-    "fees"
-    ]
-    return bills_items
-
 
 if __name__ == "__main__":
     print("Display grocery list")
     print(grocery_list())
-    print(clothing())
-    print(bills())
