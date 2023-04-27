6c9d779a0c76b66e8b78359687beda26ecf9e2b4
hhurchan <h.hurchand@gmail.com>
2023-04-27 19:07:26 -0400

    add clothing items

--- main.py
+++ main.py
@@ -7,7 +7,16 @@ def grocery_list():
                "jam"]
     return grocery
 
+def clothing():
+    clothing_list = ["shirt",
+    "dress",
+    "socks",
+    "tie"
+    ]
+    return clothing_list
+
 
 if __name__ == "__main__":
     print("Display grocery list")
     print(grocery_list())
+    print(clothing())
