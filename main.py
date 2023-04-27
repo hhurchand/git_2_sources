c095a630e16c35aec0bb1d87efd9ad29ad88de5b
hhurchan <h.hurchand@gmail.com>
2023-04-27 19:24:56 -0400

    "Removed clothing and bill items"

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ def grocery_list():
                "jam"]
     return grocery
 
-
+remove grocery list and bills.
 if __name__ == "__main__":
     print("Display grocery list")
     print(grocery_list())
